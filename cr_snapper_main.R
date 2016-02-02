76b5ff1b1654c654adc944e59866d966ca36d429
merrillrudd <merrillrudd@gmail.com>
2016-02-02 11:07:13 -0800

    base assessment with single fleet'

--- cr_snapper_main.R
+++ cr_snapper_main.R
@@ -1,10 +1,34 @@
 rm(list=ls())
 
 #########################
+## notes
+#########################
+
+## may need monthly time-steps (monthly recruitment)
+## should compare to LB-SPR and other methods
+## alternate size at first maturity: 
+	## 34 cm (Rojas 1996 Gulf of Nicoya)
+	## 23.5 cm (Colombia snappers)
+	## 2/3 linf
+
+#########################
 ## directories 
 #########################
 
 init_dir <- "C:\\Git_Projects\\cr_snapper"
+
+fig_dir <- file.path(init_dir, "figs")
+dir.create(fig_dir, showWarnings=FALSE)
+
+
+setwd(init_dir)
+source("R_functions\\functions.R")
+
+#########################
+## setup life history
+#########################
+
+cr_lh <- create_lh(species="CRSNAP", selex="asymptotic")
 
 #########################
 ## read in data + 
@@ -14,17 +38,6 @@ init_dir <- "C:\\Git_Projects\\cr_snapper"
 data_raw <- read.csv(file.path(init_dir, "cr_snapper_database.csv"), 
 	stringsAsFactors=FALSE)
 
-adjust_date <- function(date){
-	date_type <- grepl("/", date)
-	if(date_type){
-		yr <- strsplit(date, "/")[[1]][3]
-		if(nchar(yr)<4) yr <- paste0("20", yr)
-	}
-	if(date_type==FALSE) yr <- date
-
-	return(yr)
-}
-
 data_raw$Year <- sapply(1:nrow(data_raw), function(x) adjust_date(data_raw$Date[x]))
 data_raw$TL_cm <- as.numeric(data_raw$TL_cm)
 
@@ -33,15 +46,120 @@ data_raw$TL_cm <- as.numeric(data_raw$TL_cm)
 #############################
 
 lg <- data_raw[which(data_raw$Species=="Lutjanus guttatus"),]
+write.csv(lg, file.path(init_dir, "cr_snapper_filtered.csv"))
 
-## category vectors
-years <- unique(as.numeric(lg$Year))[order(unique(as.numeric(lg$Year)))]
-gears <- unique(lg$Gear)
+#############################
+## subset by gears
+#############################
+setwd(init_dir)
+source("R_functions\\functions.R")
+
+lg_bl <- lg[which(lg$Gear=="Bottom Longline"),]
+lg_g <- lg[which(lg$Gear=="Gillnet"),]
 
 ## annual mean length
-ml_lg <- sapply(1:length(years), function(x) mean(lg$TL_cm[which(as.numeric(lg$Year)==years[x])], na.rm=TRUE))
-plot(x=years, y=ml_lg, ylim=c(0,max(ml_lg)*1.1), xlab="Year", ylab="TL (cm)", pch=19)
+png(file=file.path(fig_dir, "mean_length_catch.png"), width=6, height=5, units="in", res=200)
+ml <- mean_length(data=lg, plot=TRUE)
+dev.off()
 
-## percentage of data by gear
-ng_lg <- sapply(1:length(gears), function(x) nrow(lg[which(lg$Gear==gears[x]),])/nrow(lg))
-names(ng_lg) <- gears
+###############################
+## length frequency
+###############################
+
+setwd(init_dir)
+source("R_functions\\functions.R")
+
+png(file=file.path(fig_dir, "length_frequency_catch.png"), width=9, height=7, units="in", res=200)
+lf <- length_frequency(binwidth=1, linf=cr_lh$linf,
+ lmat=cr_lh$Lmat, data=lg, plot=TRUE, weight=TRUE)
+dev.off()
+
+
+#################################
+## catch and effort
+#################################
+setwd(init_dir)
+source("R_functions\\functions.R")
+
+fishery_data <- catch_effort(data=lg, plot=TRUE)
+catch <- fishery_data$catch
+cpue <- fishery_data$cpue
+obs_per_yr <- fishery_data$obs_per_yr
+years <- fishery_data$years
+
+#######################
+## data setup
+#######################
+
+setwd(init_dir)
+source("R_functions\\functions.R")
+
+catch_yrs <- which(is.na(catch)==FALSE)
+cpue_yrs <- which(is.na(cpue)==FALSE)
+lf_years <- which(years %in% rownames(lf)[which(rowSums(lf)>0)])
+
+catch_input <- catch[which(is.na(catch)==FALSE)]
+cpue_input <- cpue[which(is.na(cpue)==FALSE)]
+lf_input <- lf[which(rowSums(lf)>0),]
+obs_input <- obs_per_yr
+
+names(catch_input) <- catch_yrs
+names(cpue_input) <- cpue_yrs
+rownames(lf_input) <- lf_years
+
+########################################
+## length-based state-space assessment
+########################################
+
+library(TMB)
+run_exe <- file.path(init_dir, "inst", "executables")
+version <- "lb_statespace"
+setwd(run_exe)
+dyn.unload(paste0(version, ".dll"))
+# file.remove(paste(version, c(".dll", ".o"), sep=""))
+# compile(paste0(version, ".cpp"))
+
+##########
+## base
+##########
+
+model_name <- "base"
+dat_input <- create_inputs(simdir=model_name, param=FALSE, val=FALSE, lh_dat=cr_lh)
+
+model_dir <- file.path(init_dir, model_name)
+if(file.exists(model_dir)) unlink(model_dir, TRUE)
+dir.create(model_dir, showWarnings=FALSE)
+
+lh <- dat_input
+catch <- catch_input
+index <- cpue_input
+lengthfreq <- lf_input
+obs_per_yr <- obs_input
+dat_avail <- c("lengthfreq_multiple", "catch_total", "index_total")
+version <- "lb_statespace"
+obs_meanlen=ml$all_gears
+
+setwd(init_dir)
+source("R_functions\\functions.R")
+
+base_stsp <- run_statespace(lh=dat_input, years=years, catch=catch_input, 
+	index=cpue_input, lengthfreq=lf_input, obs_per_yr=obs_input, 
+	dat_avail=c("lengthfreq_multiple", "catch_total", "index_total"),
+	model_name=model_name, model_dir=model_dir, obs_meanlen=ml$all_gears,
+	est_params=c("log_F_t_input", "log_q_I", "beta", "log_sigma_R", "S50", "CV_l", "CV_c"))
+
+base_stsp_rep <- readRDS(file.path(init_dir, model_name, "Report.rds"))
+
+### fix CV_c
+model_name <- "base_v2"
+model_dir <- file.path(init_dir, model_name)
+if(file.exists(model_dir)) unlink(model_dir, TRUE)
+dir.create(model_dir, showWarnings=FALSE)
+
+base_stsp_v2 <- run_statespace(lh=dat_input, years=years, catch=catch_input, 
+	index=cpue_input, lengthfreq=lf_input, obs_per_yr=obs_input, 
+	dat_avail=c("lengthfreq_multiple", "catch_total", "index_total"),
+	model_name=model_name, model_dir=model_dir, obs_meanlen=ml$all_gears,
+	est_params=c("log_F_t_input", "log_q_I", "beta", "log_sigma_R", "S50", "CV_l"))
+
+base_stsp_v2_rep <- readRDS(file.path(init_dir, model_name, "Report.rds"))
